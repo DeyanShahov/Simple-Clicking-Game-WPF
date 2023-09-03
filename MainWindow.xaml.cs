d5eeba767541e8609243a9a606bc69734eca310e
redfoxbg <redfoxbg@gmail.com>
2023-09-03 16:57:44 +0300

    Adding Game loop, ClickEvent and some basic setings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Simple_Clicking_Game_WPF
 {
@@ -20,9 +21,88 @@ namespace Simple_Clicking_Game_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        List<Ellipse> removeThis = new List<Ellipse>();
+
+        int spawnRate = 60;
+        int currentRate;
+        int lastScore = 0;
+        int health = 350;
+        int posX;
+        int posY;
+        int score = 0;
+
+        double growthRate = 0.5;
+
+        MediaPlayer playClickSound = new MediaPlayer();
+        MediaPlayer playerPopSound = new MediaPlayer();
+
+        Uri ClickedSound;
+        Uri PoppedSound;
+
+        Brush brush;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+
+            currentRate = spawnRate;
+
+            ClickedSound = new Uri("pack://siteoforigin:,,,/Sound/clickedpop.mp3");
+            PoppedSound = new Uri("pack://siteoforigin:,,,/Sound/pop.mp3");
+        }
+
+        private void GameLoop(object? sender, EventArgs e)
+        {
+            txtScore.Content = "Score: " + score;
+            txtLastScore.Content = "Last Score: " + lastScore;
+
+            currentRate -= 2;
+
+            if (currentRate < 1)
+            {
+                currentRate = spawnRate;
+
+                posX = random.Next(15, 700);
+                posY = random.Next(50, 350);
+
+                brush = new SolidColorBrush(Color.FromRgb((byte)random.Next(1, 255), (byte)random.Next(1, 255), (byte)random.Next(1, 255)));
+
+                Ellipse circle = new Ellipse
+                {
+                    Tag = "circle",
+                    Height = 10,
+                    Width = 10,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    Fill = brush
+                };
+
+                Canvas.SetLeft(circle, posX);
+                Canvas.SetTop(circle, posY);
+
+                MyCanvas.Children.Add(circle);
+            }
+        }
+
+        private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Ellipse)
+            {
+                Ellipse circle = (Ellipse)e.OriginalSource;
+
+                MyCanvas.Children.Remove(circle);
+
+                score++;
+
+                playClickSound.Open(ClickedSound);
+                playClickSound.Play();
+            }
         }
     }
 }
